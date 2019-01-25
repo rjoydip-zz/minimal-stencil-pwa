0e209f6d8d588004c0a135dd5e4aba6bfc3c2692
Joydip Roy <joydipand@gmail.com>
2019-01-25 15:11:06 +0530

    initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  globalStyle: 'src/global/app.css',
+  globalScript: 'src/global/app.ts',
+  outputTargets: [
+    {
+      type: 'www',
+      dir: "public",
+      serviceWorker: {
+        swSrc: 'src/sw.js',
+        globPatterns: [
+          '**/*.{html,js,css,json,ico,png}'
+        ]
+      }
+    }
+  ],
+  copy: [
+    { src: 'favicon.ico'}
+  ]
+};
